747251aac2b04efbca5eef7e963f4a98171e0029
xiaoguai233 <xiaoguai233.126.com>
2020-03-16 18:22:12 +0800

    完成了分类参数的开发

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -51,9 +51,12 @@
             <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
             <el-table-column label="操作" width="300px">
                 <template slot-scope="scope">
-                    <el-button size="mini" type="primary" icon="el-icon-edit" @click="rolesEditDialog(scope.row.id)" >编辑</el-button>
-                    <el-button size="mini" type="danger" icon="el-icon-delete" @click="rolesremoveDialog(scope.row.id)">删除</el-button>
-                    <el-button size="mini" type="warning" icon="el-icon-setting" @click="showSetRightDialog(scope.row)">分配权限</el-button>
+                    <el-button size="mini" type="primary" icon="el-icon-edit" 
+                    @click="rolesEditDialog(scope.row.id)" >编辑</el-button>
+                    <el-button size="mini" type="danger" icon="el-icon-delete" 
+                    @click="rolesremoveDialog(scope.row.id)">删除</el-button>
+                    <el-button size="mini" type="warning" icon="el-icon-setting" 
+                    @click="showSetRightDialog(scope.row)">分配权限</el-button>
                 </template>
             </el-table-column>
         </el-table>
--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 
 import Cate from '../components/goods/Cate.vue'
-// import Params from '../components/goods/Params.vue'
+import Params from '../components/goods/Params.vue'
 
 // import GoodsList from '../components/goods/List.vue'
 // import Add from '../components/goods/Add.vue'
@@ -50,7 +50,7 @@ const routes = [
       { path: '/rights', component: Rights },
       { path: '/roles', component: Roles },
       { path: '/categories', component: Cate },
-      // { path: '/params', component: Params },
+      { path: '/params', component: Params },
       // { path: '/goods', component: GoodsList },
       // { path: '/goods/add', component: Add },
       // { path: '/orders', component: Order },
